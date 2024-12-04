611d42119da84f2402a5f5e00d970dc5c2ae26f3
leanhvu21042001 <leanhvu21042001@gmail.com>
2024-12-04 10:15:34 +0700

    feat: get list blog

---
+++ src/application/usecases/get-list-blog/get-list-blog.usecase.ts
@@ -0,0 +1,53 @@
+import { BlogEntity } from '~/domain/entities/blog.entity'
+import { TagEntity } from '~/domain/entities/tag.entity'
+import { BlogRepository } from '~/domain/repositories/blog.repository'
+
+import { IUseCase } from '../usecase.interface'
+
+export type GetListBlogInputDto = void
+
+export type GetListBlogOutputDto = {
+  blogs: Array<{
+    createdAt: Date
+    updatedAt: Date
+    deletedAt: Date | null
+    id: string
+    title: string
+    summary: string
+    content: string
+    draft: boolean
+    tags: TagEntity[]
+  }>
+}
+
+export class GetListBlogUseCase implements IUseCase<GetListBlogInputDto, GetListBlogOutputDto> {
+  private constructor(private readonly blogRepository: BlogRepository) {}
+
+  public static create(productRepository: BlogRepository): GetListBlogUseCase {
+    return new GetListBlogUseCase(productRepository)
+  }
+
+  async execute(): Promise<GetListBlogOutputDto> {
+    const blogs = await this.blogRepository.list()
+
+    const output = this.presentOutput(blogs)
+
+    return output
+  }
+
+  private presentOutput(blogs: BlogEntity[]): GetListBlogOutputDto {
+    return {
+      blogs: blogs.map((blog) => ({
+        createdAt: blog.createdAt,
+        updatedAt: blog.updatedAt,
+        deletedAt: blog.deletedAt,
+        id: blog.id,
+        title: blog.title,
+        summary: blog.summary,
+        content: blog.content,
+        draft: blog.draft,
+        tags: blog.tags,
+      })),
+    }
+  }
+}
